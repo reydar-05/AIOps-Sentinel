e77febefc8d5acb91f77e0e0e04809a9f675ce2b
Renu Gopu <renu05.gopu@gmail.com>
2026-03-24 02:53:48 +0530

    fix: expand lambda variable definitions to multiline HCL

--- terraform/modules/lambda/variables.tf
+++ terraform/modules/lambda/variables.tf
@@ -7,5 +7,13 @@ variable "bedrock_model_id" { type = string }
 variable "dynamodb_table_name" { type = string }
 variable "s3_log_bucket" { type = string }
 variable "slack_secret_name" { type = string }
-variable "groq_api_key" { type = string; default = ""; sensitive = true }
-variable "bedrock_max_tokens" { type = string; default = "2048" }
+variable "groq_api_key" {
+  type      = string
+  default   = ""
+  sensitive = true
+}
+
+variable "bedrock_max_tokens" {
+  type    = string
+  default = "2048"
+}
